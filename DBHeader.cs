5dd9b534eb1e3a26c199de04086714dc8f3db5e1
rhoomse <rhoomse@gmail.com>
2021-10-23 00:36:47 +0300

    + костяк дб

---
+++ DBHeader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HACK_PTS
+{
+    class DBHeader
+    {
+        private ulong size;
+        public DBHeader(ulong size)
+        {
+            this.size = size;
+        }
+    }
+}
---
+++ RHeader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HACK_PTS
+{
+    class RHeader
+    {
+        ulong id;
+        ushort size;
+        byte[] data;
+        public RHeader(ushort size)
+        {
+            this.size = size;
+        }
+    }
+}
