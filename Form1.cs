25e575ae0e1ac88564f91e938b3418542c8e3a7e
loinos <PavlovskiyD.V@yandex.ru>
2021-10-23 15:17:59 +0300

    add hemminc code

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,119 @@ namespace HACK_PTS
         public Form1()
         {
             InitializeComponent();
+        }
+        public string bitarraytoprint (BitArray bits)
+        {
+            string ans = "";
+            foreach(bool item in bits)
+            {
+                if (item)
+                {
+                    ans += "1";
+                }
+                else
+                {
+                    ans += "0";
+                }
+            }
+
+            return ans;
+        }
+        public BitArray addbit(BitArray bits)
+        {
+            BitArray bit_array = new BitArray(21);
+            int j = 0;
+            for (int i = 0; i < 21; i++)
+            {
+                if(i == 0 | i == 1 | i == 3 | i == 7 | i == 15)
+                {
+                    bit_array[i] = false;
+                }
+                else
+                {
+                    bit_array[i] = bits[j];
+                    j++;
+                }
+            }
+            int controlsum = 0;
+            for(int i = 0; i < 21; i +=2)
+            {
+                if (bit_array[i])
+                {
+                    controlsum++;
+                }
+            }
+            if( controlsum % 2 != 0)
+            {
+                bit_array[0] = true;
+            }
+            controlsum = 0;
+            for(int i = 1; i < 21; i += 3)
+            {
+                for(int k = 0; k < 2; k++)
+                {
+                    if (bit_array[i+k])
+                    {
+                        controlsum++;
+                    }
+                }
+            }
+            if (controlsum % 2 != 0)
+            {
+                bit_array[1] = true;
+            }
+            controlsum = 0;
+            for (int i = 3; i < 21; i += 5)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    if (k+i<21 && bit_array[i + k])
+                    {
+                        controlsum++;
+                    }
+                }
+            }
+            if (controlsum % 2 != 0)
+            {
+                bit_array[3] = true;
+            }
+            controlsum = 0;
+            for (int i = 15; i < 21; i ++)
+            {
+                if (bit_array[i])
+                {
+                    controlsum++;
+                }
+            }
+            if (controlsum % 2 != 0)
+            {
+                bit_array[15] = true;
+            }
+            for (int i = 7; i < 16; i++)
+            {
+                if (bit_array[i])
+                {
+                    controlsum++;
+                }
+            }
+            if (controlsum % 2 != 0)
+            {
+                bit_array[7] = true;
+            }
+            return bit_array;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str1 = richTextBox1.Text;
+            byte[] str_bin = System.Text.Encoding.UTF8.GetBytes(str1);
+            BitArray bits = new BitArray(str_bin);
+
+            //код хемминга
+           
+
+
+            richTextBox2.Text = bitarraytoprint(addbit(bits));
+
         }
     }
 }
