c146f787d3794d3fb7e069093b6cd236b5b6110e
rhoomse <rhoomse@gmail.com>
2021-10-24 04:29:20 +0300

    +add

--- h/Api.h
+++ h/Api.h
@@ -1,12 +1,30 @@
 class Api {
     Database *db;
     std::string path;
-    
+
+    // Заполнение блока
+    int FillBlock(std::fstream &iof, unsigned char array[], int size, int id) {
+        uint16_t fill = 1;
+
+        iof.write((char*)&id, sizeof(id));
+        iof.write((char*)&fill, sizeof(fill));
+        iof.write((char*)&size, sizeof(size));
+        for (int i = 0; i < size; ++i) {
+            unsigned char b = array[i];
+            iof.write((char*)&b, sizeof(b));
+        }
+        iof.close();
+        return id;
+    }
 public:
+
+    // Говори адрес
     Api(std::string path) {
         this->path = path;
         db = new Database();
     }
+
+    // Обязательно перед заполнением базы. Задает метаданные базы
     void New() {
         std::ofstream out(path, std::ios::binary | std::ios::out);
         uint16_t identification = Database::IDENTIFICATION;
@@ -19,17 +37,37 @@ public:
     int Set(unsigned char array[], int size){
         std::fstream iof(path, std::ios::binary | std::ios::out | std::ios::in);
         iof.seekp(Database::HEADER, std::ios_base::beg);
+
+        // В порядке нахождения в базе
+        uint64_t id = 0;
+        uint16_t fill = 1;
+        uint64_t c_size = size;
+
+        // Заполнение первого элемента
         if (iof.eof()) {
-            iof.seekp(Record::HEADER);
-            for (int i = 0; i < size; ++i) {
-                unsigned char b = array[i];
-                iof.write((char*)&b, sizeof(b));
-            }
+            return FillBlock(iof, array, size, id);
         }
+
+        // Блин опасно
         while (iof.eof()) {
 
+            // Получаем текущий коммит
+            iof.read((char*)&id, sizeof(id));
+            iof.read((char*)&fill, sizeof(fill));
+            iof.read((char*)&c_size, sizeof(c_size));
+
+            // Ошибка или блок пуст
+            if(fill == 0)
+                // Ошибка или свободный блок
+                // Если свободен - заполняем
+                if (size < c_size) {
+                    return FillBlock(iof, array, size, id);
+                }
         }
-        return 0;
+
+        // Если конец фаила - аппенд
+        return FillBlock(iof, array, size, id);
+
     }
     Record Get(uint64_t id){
 
--- h/Record.h
+++ h/Record.h
@@ -2,16 +2,20 @@
 
 class Record {
     struct HRecord {
+        uint64_t id;
+        uint16_t fill;
+        uint64_t size;
         std::vector<unsigned char> data;
-        HRecord(){
+        HRecord(uint64_t size){
             data = std::vector<unsigned char>();
+            this->size = size;
         }
     };
     HRecord *hr;
-    static const uint16_t HEADER = 6;
+    static const uint16_t HEADER = 14;
 public:
-    Record(){
-        hr = new HRecord();
+    Record(uint64_t size){
+        hr = new HRecord(size);
     }
     friend class Api;
     unsigned char& operator[](const int i) {
