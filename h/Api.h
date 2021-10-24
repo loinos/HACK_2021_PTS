e7a09abd4a50410a8047d5b94e8610213b128ca7
rhoomse <rhoomse@gmail.com>
2021-10-24 04:53:09 +0300

    +Get

--- h/Api.h
+++ h/Api.h
@@ -70,7 +70,31 @@ public:
 
     }
     Record Get(uint64_t id){
+        std::ifstream in(path, std::ios::binary | std::ios::in);
+        in.seekg(Database::HEADER, std::ios_base::beg);
 
+        // Сдвиг, сколько бит пропускать
+        // на каждой итерации
+        uint16_t shift = 0;
+
+        // Текущее положение каретки
+        uint64_t c_id = -1;
+
+        while (in.eof()) {
+
+            // Если текущее положение искомое - возврат
+            in.read((char*)&c_id, sizeof(c_id));
+            if (c_id == id) return id;
+
+            // Иначе сдвиг == 16 + 64 = 80 бит => 10 байт
+            //uint64_t id; 64 = 8
+            //uint16_t fill; 16 = 2
+            //uint64_t c_size; 64 = 8
+            shift = sizeof(uint16_t) + sizeof(uint64_t);
+            in.seekg(shift);
+        }
+
+        return -1;
     }
     void Update(uint64_t id){
 
